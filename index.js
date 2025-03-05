0774a3cb05e474ad1af7c4a8bfc625eef812e4f9
M.P.C PRIYAMANTHA <95483538+ChandimalPriyamantha@users.noreply.github.com>
2025-03-06 02:23:47 +0530

    develop user apis

--- index.js
+++ index.js
@@ -133,6 +133,44 @@ app.get('/user/:id', async (req, res) => {
   res.send(user);
 });
 
+ // GET USER BY EMAIL
+ app.get('/user/:email', async (req, res) => {
+  const email = req.params.email;
+  const query = { email: email };
+  const result = await userCollection.findOne(query);
+  res.send(result);
+})
+
+ // Delete a user
+
+ app.delete('/delete-user/:id',async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await userCollection.deleteOne(query);
+  res.send(result);
+})
+// UPDATE USER
+app.put('/update-user/:id', async (req, res) => {
+  const id = req.params.id;
+  const updatedUser = req.body;
+  const filter = { _id: new ObjectId(id) };
+  const options = { upsert: true };
+  const updateDoc = {
+      $set: {
+          name: updatedUser.name,
+          email: updatedUser.email,
+          role: updatedUser.option,
+          address: updatedUser.address,
+          phone: updatedUser.phone,
+          about: updatedUser.about,
+          photoUrl: updatedUser.photoUrl,
+          skills: updatedUser.skills ? updatedUser.skills : null,
+      }
+  }
+  const result = await userCollection.updateOne(filter, updateDoc, options);
+  res.send(result);
+})
+
 // Root route
 app.get('/', (req, res) => {
   res.send('Hello World!');
